3eb84e857e499a97de8886749ae1c73d7598600a
Hrishikesh Naramparambath <hrishabari@gmail.com>
2021-11-14 20:21:19 +0530

    'Added the ability to browse through file explorer'

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,73 +1,67 @@
 # importing necessary modules
 
-from pygame import mixer
 import tkinter as tk
+from tkinter import filedialog
+from pygame import mixer
+import os
+
+mainColor = '#17202A'
 
 window = tk.Tk()
-window.title("BasicCalc")
-window.configure(bg='#581845')
+window.title("Giosoco")
+window.configure(bg=mainColor)
 window.geometry("500x500")
 
 mixer.init()
 
 global dictionary
-dictionary = {'Ab Tere Bin': 'D:\Music\Aashiqui\Ab Tere Bin.mp3',
-              'Ek Sanam Chahiye': 'D:\Music\Aashiqui\Ek Sanam Chahiye.mp3'}
+dictionary = {'E.S. Posthumus - Nara': 'D:\Music\Independent\E.S. Posthumus - Nara.mp3',
+              'Against All Odds': "D:\Music\Moonlight Shadows\\05 Against All Odds.mp3"}
 
 
 def play(*args):
-    song = list.get('anchor')
-    mixer.music.load(dictionary[song])
-    mixer.music.play()
-
+    try:
+        song = list.get('anchor')
+        mixer.music.load(dictionary[song])
+        mixer.music.play()
+    except:
+        pass
 
 def pauseplay():
     if mixer.music.get_busy() == True:
         mixer.music.pause()
         PausePlay.configure(text='Play')
     else:
-        mixer.music.unpause()
-        PausePlay.configure(text='Pause')
+        pass
 
-
-
-
-def addSongs():
-    addSongEntry.configure(state='normal')
-    addSongPath.configure(state='normal')
-
-
-def ok():
-    x = list.size()
-    list.insert(x, addSongEntry.get())
-    path = addSongPath.get()
-    path = path.replace('"', '')
-    dictionary[addSongEntry.get()] = path
-
+def browseSong():
+    file = filedialog.askopenfilenames()
+    browsePath = str(file)[2:len(str(file))-3]
+    mixer.music.load(browsePath)
+    mixer.music.play()
+    songName = os.path.basename(browsePath)
+    dictionary[songName] = browsePath
+    y = list.size()
+    list.insert(y, songName)
 
 Play = tk.Button(text='Play from Beginning', command=play)
 Play.grid(row=0, column=1)
-PausePlay = tk.Button(text='Pause', command=pauseplay)
+PausePlay = tk.Button(text='Pause', command=pauseplay, width=10)
 PausePlay.grid(row=0, column=2)
-addSongs = tk.Button(text='Add Songs', command=addSongs)
-addSongs.grid(row=1, column=2)
-ok = tk.Button(text='OK', command=ok)
-ok.grid(row=4, column=2)
 
-addSongEntry = tk.Entry()
-addSongEntry.grid(row=2, column=2)
-addSongEntry.configure(state='disabled')
-addSongEntry.insert(0, "Enter song name")
+Box = tk.LabelFrame(window, text='ADD SONGS', font='Courier 12')
+Box.grid(column=0, row=3)
 
-addSongPath = tk.Entry()
-addSongPath.grid(row=3, column=1)
-addSongPath.configure(state='disabled')
-addSongPath.insert(0, "Enter song path")
+emptyLabel = tk.Label(bg=mainColor)
+emptyLabel.grid(column=0, row=2)
+
+browseSongs = tk.Button(Box, text='Browse', width=16, command=browseSong)
+browseSongs.grid(row=2, column=0)
 
 list = tk.Listbox(window)
 list.grid(row=1, column=0)
-list.insert(0, 'Ab Tere Bin')
-list.insert(1, 'Ek Sanam Chahiye')
+list.insert(0, 'E.S. Posthumus - Nara')
+list.insert(1, 'Against All Odds')
 list.bind('<<ListboxSelect>>', play)
 
 window.mainloop()
